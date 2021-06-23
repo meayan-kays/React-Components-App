84cb8beadf641216703179325e881b91e3ff3dfc
Kays Meayan <80474123+meayan-kays@users.noreply.github.com>
2021-06-23 14:21:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Apps from './Apps';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Apps />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
