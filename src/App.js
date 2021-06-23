0383ea529b2cb4d1b1259bf552d5a3880ac40b97
Kays Meayan <80474123+meayan-kays@users.noreply.github.com>
2021-06-23 14:25:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Address from './Components/Profile/Address'
+import FullName from './Components/Profile/FullName'
+import ProfilPhoto from './Components/Profile/ProfilPhoto'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Apps">
+            <ProfilPhoto />
+            <FullName />
+            <Address />            
+        </div>
+    )
 }
 
-export default App;
+export default App
